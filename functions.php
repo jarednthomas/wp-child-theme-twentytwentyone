0917cd5096576441bab7857a783cf61698297b6d
Jared <jaredpyrothomas@gmail.com>
2021-10-21 22:58:39 -0500

    minor updates

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() 
 {
     wp_enqueue_style( 
-        'child-style', 
+        'my-theme', 
         get_stylesheet_uri(),
         array( 'twenty-twenty-one-style' ), 
         wp_get_theme()->get('Version')
